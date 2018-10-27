e02d5361f1aba18acb89e7edd72d682675c0639d
shoto.mac <shotolee@gmail.com>
2018-10-27 13:33:56 +0800

    不知道改了什么

---
+++ Problem3/3.rb
@@ -0,0 +1,18 @@
+n = 600851475143
+m = n ** (1 / 2)
+
+
+if n % (n ** (1/2)) != 0
+
+
+
+
+
+for i in 1 .. n do
+    if (n % (n-i) == 0 && (n-i) != 1 ) then
+        p n
+        next
+   # else
+    #    puts n
+    end
+end
